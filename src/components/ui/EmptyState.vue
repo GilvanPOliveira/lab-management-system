7d4717d7bf533832d6ac08d673a92a674918c3c3
Gilvan Oliveira <gilvanoliveira06@gmail.com>
2026-03-30 14:27:14 -0300

    feat: add empty state component

---
+++ src/components/ui/EmptyState.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <header class="space-y-2">
+    <h1 class="text-3xl font-semibold tracking-tight">{{ title }}</h1>
+    <p class="text-sm text-zinc-400">
+      {{ description }}
+    </p>
+  </header>
+</template>
